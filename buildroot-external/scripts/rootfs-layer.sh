9cfd40ecff7f21ade2165c1425c57938ded66d3d
Nikita Maslo <nikitamalco203@gmail.com>
2025-11-07 12:24:43 +0300

    remove init scripts

--- buildroot-external/scripts/rootfs-layer.sh
+++ buildroot-external/scripts/rootfs-layer.sh
@@ -2,11 +2,8 @@
 
 function fix_rootfs() {
     chmod +x "${BOARD_DIR}/rootfs-overlay/etc/wpa_action.sh"
-    chmod +x "${BOARD_DIR}/rootfs-overlay/etc/hotplug.d/iface/10-dhcp"
     chmod +x "${BOARD_DIR}/rootfs-overlay/etc/init.d/S02fixnetwork"
-    chmod +x "${BOARD_DIR}/rootfs-overlay/etc/init.d/wifi_setup.sh"
 }
-
 
 function install_tini_docker() {
     ln -fs /usr/bin/tini "${TARGET_DIR}/usr/bin/docker-init"
