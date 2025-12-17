da95571e63b71035a9b0c78295065145c0aef0d0
Nikita Maslo <nikitamalco203@gmail.com>
2025-12-17 12:05:21 +0300

    Fix genimage and overlay init scripts

--- buildroot-external/board/jethome/jethub-j200/os-hook.sh
+++ buildroot-external/board/jethome/jethub-j200/os-hook.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 # shellcheck disable=SC2155
 
-# shellcheck source=../../../scripts/burn.sh
-. "${SCRIPT_DIR}/burn.sh"
-
 function os_pre_image() {
     local BOOT_DATA="$(path_boot_dir)"
     dd if=/dev/zero of="${BINARIES_DIR}/env.blank" bs=64K count=1
     lzma -f -k -9 "${BINARIES_DIR}/Image"
 
-    cp "${BINARIES_DIR}/boot.scr" "${BOOT_DATA}/boot.scr"
     mkdir -p "${BOOT_DATA}/amlogic"
     cp "${BINARIES_DIR}/meson-sm1-jethome-jethub-j200.dtb" "${BOOT_DATA}/amlogic/"
     if ls "${BINARIES_DIR}"/*.dtbo 1> /dev/null 2>&1; then
@@ -17,13 +13,4 @@ function os_pre_image() {
         mkdir -p "${BOOT_DATA}/overlays"
         cp "${BINARIES_DIR}"/*.dtbo "${BOOT_DATA}/overlays/"
     fi
-    cp "${BOARD_DIR}/boot-env.txt" "${BOOT_DATA}/os-config.txt" || true
-    cp "${BOARD_DIR}/cmdline.txt" "${BOOT_DATA}/cmdline.txt"
-}
-
-function os_post_image() {
-    convert_disk_image_xz
-    # support for create AmLogic burnable images
-    [[ -f "${BINARIES_DIR}/platform.conf" ]] && _create_disk_burn
-    [[ -f "${BINARIES_DIR}/platform.conf" ]] && convert_disk_image_burn_zip
 }
--- buildroot-external/board/jethome/jethub-j200/rootfs-overlay/etc/wpa_action.sh
+++ buildroot-external/board/jethome/jethub-j200/rootfs-overlay/etc/wpa_action.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 IFACE="$1"
 CMD="$2"
