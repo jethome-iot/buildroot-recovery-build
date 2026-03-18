5afe605d43866641aeef46322bf982b8f733ca5b
Nikita Maslo <nikitamalco203@gmail.com>
2026-03-18 15:31:25 +0300

    Remove j200 files

--- buildroot-external/board/jethome/jethub-j200/rootfs-overlay/etc/wpa_action.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-IFACE="$1"
-CMD="$2"
-
-[ "$CMD" = "CONNECTED" ] || exit 0
-logger -t wifi "CONNECTED on $IFACE — requesting DHCP"
-udhcpc -i "$IFACE" -q -n -b > /dev/null 2>&1
