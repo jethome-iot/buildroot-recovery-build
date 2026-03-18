b10e6929dcfaa3bec1fa8bbaa613a7f779e3ac63
Nikita Maslo <nikitamalco203@gmail.com>
2026-03-18 16:49:31 +0300

    Add j300 files

---
+++ buildroot-external/board/jethome/jethub-j300/os-hook.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# shellcheck disable=SC2155
+
+function os_pre_image() {
+    local BOOT_DATA="$(path_boot_dir)"
+    dd if=/dev/zero of="${BINARIES_DIR}/env.blank" bs=64K count=1
+    lzma -f -k -9 "${BINARIES_DIR}/Image"
+
+    mkdir -p "${BOOT_DATA}/amlogic"
+    cp "${BINARIES_DIR}/meson-s7d-jethub-j300.dtb" "${BOOT_DATA}/amlogic/"
+    if ls "${BINARIES_DIR}"/*.dtbo 1> /dev/null 2>&1; then
+        echo "Found .dtbo files in ${BINARIES_DIR}"
+        mkdir -p "${BOOT_DATA}/overlays"
+        cp "${BINARIES_DIR}"/*.dtbo "${BOOT_DATA}/overlays/"
+    fi
+}
