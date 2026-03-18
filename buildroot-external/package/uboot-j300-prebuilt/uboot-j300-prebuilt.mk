69ac7f74ccf34f89fa68abb597848036654f523b
Nikita Maslo <nikitamalco203@gmail.com>
2026-03-18 15:25:49 +0300

    Remove useless OS packages, add u-boot bin files for test

---
+++ buildroot-external/package/uboot-j300-prebuilt/uboot-j300-prebuilt.mk
@@ -0,0 +1,23 @@
+################################################################################
+#
+# uboot-j300-prebuilt
+#
+################################################################################
+
+UBOOT_J300_PREBUILT_VERSION = 1.0
+UBOOT_J300_PREBUILT_SITE = $(BR2_EXTERNAL_JHOS_PATH)/package/uboot-j300-prebuilt
+UBOOT_J300_PREBUILT_SITE_METHOD = local
+UBOOT_J300_PREBUILT_INSTALL_IMAGES = YES
+UBOOT_J300_PREBUILT_LICENSE = Proprietary
+UBOOT_J300_PREBUILT_REDISTRIBUTE = NO
+
+# Always reinstall images (copy from source, not build dir)
+define UBOOT_J300_PREBUILT_INSTALL_IMAGES_CMDS
+	cp -f $(UBOOT_J300_PREBUILT_SITE)/u-boot.bin $(BINARIES_DIR)/
+	cp -f $(UBOOT_J300_PREBUILT_SITE)/u-boot.bin.sd.bin $(BINARIES_DIR)/
+endef
+
+# Force rebuild on each invocation
+UBOOT_J300_PREBUILT_ALWAYS_BUILD = YES
+
+$(eval $(generic-package))
