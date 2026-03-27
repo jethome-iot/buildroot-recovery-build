7795355f7681c369e11df3515d13d6b96c600845
Nikita Maslo <nikitamalco203@gmail.com>
2026-03-27 14:07:41 +0300

    Update rauc

--- buildroot-external/scripts/rauc.sh
+++ buildroot-external/scripts/rauc.sh
@@ -1,13 +1,6 @@
 function install_bootloader_config() {
     if [ "${BOOTLOADER}" == "uboot" ]; then
-        # fw_env.config for fw_printenv/fw_setenv (u-boot-tools).
-        # Note: Linux may expose the whole SPI-NOR as mtd0 (master device),
-        # and partitions as mtd1..N (mtd0=spi0.0, mtd1=uboot, mtd2=env, ...).
-        if [ "${BOARD_ID}" = "jethub-d2" ]; then
-            echo "/dev/mtd2 0x0 0x10000" > "${TARGET_DIR}/etc/fw_env.config"
-        else
-            # shellcheck disable=SC1117
-            echo "/dev/mtd3 0x0 0x10000" > "${TARGET_DIR}/etc/fw_env.config"
-        fi
+        # eMMC user area, offset 3.5 MiB (0x380000), size 64 KiB
+        echo "/dev/mmcblk0 0x380000 0x10000" > "${TARGET_DIR}/etc/fw_env.config"
     fi
 }
